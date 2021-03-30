9f19c5d2c382c96525440c18306749f21d0bdbbb
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-03-30 20:20:23 +0200

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+// DOM elements
+const hex = document.getElementById("hex");
+const instruction = document.querySelector("span");
+
+// Get color
+function getColor() {
+  let color = "#" + Math.random().toString(16).slice(2, 8);
+
+  document.body.style.background = color;
+  hex.innerHTML = color;
+
+  hex.style.color = "white";
+  instruction.style.color = "white";
+  instruction.style.boxShadow = "inset 0 -0.475em #262626";
+}
+
+// Change background color when spacebar is pressed
+document.body.onkeyup = (e) => {
+  if (e.keyCode == 32 || e.which == 32) {
+    getColor();
+  }
+};
